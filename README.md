0094d871852db5e1356aeb6fbc4dd3906da7ce93
Jason Cheatham <jason@jasoncheatham.com>
2021-09-01 22:37:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CS 2800 Playground
+
+This repo contains a test server and a `src/` directory.
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { join } = require('path');
+const { createServer } = require('livereload');
+const connectLiveReload = require('connect-livereload');
+
+const srcDir = join(__dirname, '..', 'src');
+
+// The LiveReload server watches for file changes and notifies the browser
+const lrServer = createServer();
+lrServer.watch(srcDir);
+
+// The express app is the main server
+const app = express();
+
+// connectLiveReload should come before the static and dynamnic routes. It
+// injects a livereload script into the browser. This script connects to
+// lrServer and refreshes the page when notified of changes.
+app.use(connectLiveReload());
+app.use(express.static(srcDir));
+
+const server = app.listen(5678, () => {
+  const addr = server.address();
+  const addrString =
+    typeof addr === 'string' ? addr : `http://localhost:${addr?.port}`;
+  console.log(`Listening at ${addrString}`);
+  console.log('Press CTRL-C to exit');
+});
